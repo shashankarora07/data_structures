e9413a09e2b6030d1baff92463dc9d714f57ee1e
shashank <shashank.arora07@gmail.com>
2019-01-07 23:30:32 +0530

    divide and conquer sorting techniques/ nsquare and nlogn time complexity assorting algorithms

---
+++ insertion_sort.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+#define SIZE 5
+
+int Insertion_sort(int *, int);
+
+int main()
+{
+	int arr[SIZE];
+	int len = sizeof(arr)/sizeof(arr[0]);
+	
+	for(int i = 0; i < len; i++)
+		scanf("%d",&arr[i]);	
+
+	Insertion_sort(arr,len);
+
+	printf("*** Sorted array ***\n");		
+	for(int i = 0; i < len; i++)
+		printf("%d\n",arr[i]);
+
+	return 0;
+}
+
+int Insertion_sort(int *arr, int N)
+{
+	int key, i, k;
+	
+	for (i = 1; i < N; i++) {
+		key = arr[i];
+		k = i;
+		while(k >= 1 && arr[k-1] > key) {
+			/*swap the element and shift it to right position*/
+			arr[k] = arr[k-1];
+			k--;
+		}
+		arr[k] = key;
+	}
+}
---
+++ quick_sort.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+
+#define SIZE 9
+
+int partition(int *, int, int);
+int QuickSort(int *, int, int);
+void swap(int *, int *);
+
+int main()
+{
+    int arr[SIZE], start, end;
+    int len = sizeof(arr)/sizeof(arr[0]);
+
+    for(int i = 0; i < len; i++)
+        scanf("%d",&arr[i]);
+
+	start = 0;
+	end = len - 1;
+    QuickSort(arr,start,end);
+
+    printf("*** Sorted array ***\n");
+    for(int i = 0; i < len; i++)
+        printf("%d\n",arr[i]);	
+
+
+	return 0;
+}
+
+int QuickSort(int *arr, int start, int end)
+{
+	int pIndex;
+	if (start < end) {
+		pIndex = partition(arr,start,end);
+		QuickSort(arr,start,pIndex-1);
+		QuickSort(arr,pIndex+1,end);
+	}
+
+}
+
+int partition(int *arr, int start, int end)
+{
+	int pivot = arr[end];
+	int pIndex = start;
+
+	for (int i = start; i <= end - 1; i++) {
+		if (arr[i] <= pivot){
+			swap(&arr[i],&arr[pIndex]);
+			pIndex++;
+		}		
+	}
+	swap(&arr[pIndex],&arr[end]);
+	
+	return pIndex;
+}
+
+void swap(int* x, int* y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
