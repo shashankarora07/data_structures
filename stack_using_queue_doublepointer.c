dc88f605f8d2b7e3f171bdfbfde893014b192149
shashank <shashank.arora07@gmail.com>
2019-02-05 09:08:21 +0530

    stack using queue operation enqueue and dequeue with doublepointer

---
+++ stack_using_queue_doublepointer.c
@@ -0,0 +1,86 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct QueueNode {
+	int data;
+	struct QueueNode *next;
+}QNode;
+
+typedef struct QueueList{
+	struct QueueNode *rear;
+	struct QueueNode *front;
+}Queue;
+
+struct stack{
+	Queue *q1;
+	Queue *q2;
+};
+
+enum operations {
+	PUSH = 1,
+	POP,
+	DISPLAY,
+	EXIT = 100
+};
+
+void push();
+void pop();
+Queue * enqueue(Queue *, int);
+void dequeue(Queue **);
+void Display(Queue *);
+
+int main()
+{
+	int data = 0, choice = 0;
+	Queue *q = NULL;
+	do {
+		printf("......IMPLEMENTATION OF STACK USING QUEUES OPERATIONS........\n");
+		printf("............PUSH : Press 1..........\n");
+		printf("............POP : Press 2............\n");
+		printf("............DISPLAY : Press 3..........\n");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+			printf("Enter data : \n");
+			scanf("%d",&data);
+			case PUSH: q = enqueue(q,data);
+			break;
+		}
+	}while(choice != EXIT);
+
+	
+	return 0;
+}
+
+Queue * enqueue(Queue *q, int newdata)
+{
+	QNode *Qnewnode = (QNode *)malloc(sizeof(QNode));
+	Qnewnode->data = newdata;
+	Qnewnode->next = NULL;
+	
+	if (NULL == q) {
+		printf("First time it will come in this ONLY\n");
+		printf("Queue is not present. Now created..\n");
+		q = (Queue *)malloc(sizeof(Queue));
+		q->rear = q->front = NULL;
+	}
+	
+	if (NULL == q->rear) {
+		printf("Also comes in this first time\n");
+		q->front = q->rear = Qnewnode;
+		return q;		
+	}	
+			
+	q->rear->next = Qnewnode;
+	q->rear = q->rear->next;
+}
+
+void Display(Queue *q)
+{
+	QNode *Dp = q->front;
+	while(Dp != NULL) {
+		printf("%d ->",Dp->data);
+		Dp = Dp->next;
+	}
+	printf("NULL");
+}	
