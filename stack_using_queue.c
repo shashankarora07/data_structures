ff507bd2ca6ac4a3b56ba82d157e9a9cc35d8067
shashank <shashank.arora07@gmail.com>
2019-02-05 19:34:53 +0530

    updated

--- stack_using_queue.c
+++ stack_using_queue.c
@@ -24,16 +24,17 @@ enum operations {
 	EXIT = 100
 };
 
-void push();
-void pop();
+void Push(struct stack *, int);
+int Pop(struct stack *);
 Queue * CreateQueue();
-int enqueue(Queue *, int);
-void dequeue(Queue **);
+int Enqueue(Queue *, int);
+int Dequeue(Queue **);
 void Display(Queue *);
+int IsQueueEmpty(Queue **);
 
 int main()
 {
-	int data = 0, choice = 0;
+	int data = 0, choice = 0, ret = 0;
 	Queue *q = NULL;
 	do {
 		printf("......IMPLEMENTATION OF STACK USING QUEUES OPERATIONS........\n");
@@ -44,13 +45,42 @@ int main()
 		scanf("%d",&choice);
 		switch(choice)
 		{
-			case CREATEQUEUE: q = CreateQueue();
+			case CREATEQUEUE: 
+				if (q != NULL) {
+					printf("Queue is present\n");
+					break;
+				}
+				q = CreateQueue();
 			break;
 			case PUSH: 
 			{	
+				if (q == NULL) {
+					printf("Queue is not present. First create it\n");
+					break;
+				}
 				printf("Enter data : \n");
 				scanf("%d",&data);
-				q = enqueue(q,data);
+				Push(q, data);
+				//Enqueue(q,data);
+			}
+			break;
+			case POP:
+				{
+					if (IsQueueEmpty(&q)) {
+						printf("Queue Is empty\n");
+						break;
+					}
+					ret = Dequeue(&q);
+					printf("POP Item = %d\n",ret);
+				}
+			break;
+			case DISPLAY: 
+			{
+				if (q == NULL) {
+					printf("Queue is empty\n");
+					break;
+				}
+				Display(q);
 			}
 			break;
 		}
@@ -60,32 +90,55 @@ int main()
 	return 0;
 }
 
+
 Queue * CreateQueue()
 {
+	Queue *Q = (Queue *)malloc(sizeof(Queue));
+	if (!Q) {
+		perror("Malloc failed:");
+		return NULL;
+	}
+	Q->rear = Q->front = NULL;
 	
+	return Q;
 }
-Queue * enqueue(Queue *q, int newdata)
+
+int Enqueue(Queue *q, int newdata)
 {
 	QNode *Qnewnode = (QNode *)malloc(sizeof(QNode));
 	Qnewnode->data = newdata;
 	Qnewnode->next = NULL;
 	
-	if (NULL == q) {
-		printf("First time it will come in this ONLY\n");
-		printf("Queue is not present. Now created..\n");
-		q = (Queue *)malloc(sizeof(Queue));
-		q->rear = q->front = NULL;
-	}
 	
 	if (NULL == q->rear) {
 		printf("Also comes in this first time\n");
 		q->front = q->rear = Qnewnode;
-		return q;		
+		return 0;		
 	}	
 			
 	q->rear->next = Qnewnode;
 	q->rear = q->rear->next;
 }
+
+int Dequeue(Queue **q)
+{
+	QNode *temp = (*q)->front;
+	int data = (*q)->front->data;
+	(*q)->front = (*q)->front->next;
+	free(temp);
+	temp = NULL;	
+	
+	if ((*q)->front == NULL) {
+			(*q)->rear = NULL;
+	}
+	return data;
+}
+
+int IsQueueEmpty(Queue **Q)
+{
+		return (NULL == (*Q)->front);
+}
+
 
 void Display(Queue *q)
 {
@@ -94,5 +147,5 @@ void Display(Queue *q)
 		printf("%d ->",Dp->data);
 		Dp = Dp->next;
 	}
-	printf("NULL");
+	printf("NULL\n");
 }	
