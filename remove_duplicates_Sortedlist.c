282b9df0118a95efdfeb3a0e17b2c391f00978c4
shashank <shashank.arora07@gmail.com>
2019-06-26 20:15:42 +0530

    remove duplicates from sorted linked list

---
+++ remove_duplicates_Sortedlist.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "singly_linkedlist.h"
+
+
+void removeDuplicates_FromSortedlist(Node **head)
+{
+	Node *start = *head;
+	Node *temp = NULL;
+
+	while (start != NULL && start->next != NULL) {
+		if (start->data == start->next->data) {
+			temp = start->next;
+			if(temp->next)
+				start->next = temp->next;
+			else
+				start->next = temp;
+			free(temp);
+		} else {
+			start = start->next;
+		}
+	}
+
+}
+
+int main()
+{
+	Node *head = NULL;
+	int no = 0;
+
+	printf("enter no:\n");
+	do {
+		scanf("%d",&no);
+		if (no == 1) 
+			Insert_linkedlist(&head,no,1);
+		else
+			Insert_linkedlist(&head,no,2);
+	} while(no != 100);
+	
+	Display_linkedlist(head);
+	removeDuplicates_FromSortedlist(&head);
+	Display_linkedlist(head);
+
+	return 0;
+}
